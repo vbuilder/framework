eab959895d477f00c182ec02912cae37b1b2f18b
Adam Staněk <adam.stanek@v3net.cz>
2011-07-26 17:00:06 +0200

    Content Management System based on vBuilder framework
    Signed-off-by: Adam Staněk <adam.stanek@v3net.cz>

--- vBuilderFw/bootstrap.php
+++ vBuilderFw/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/Utils/shortcuts.php';
 
-Nette\Environment::getConfigurator()->defaultServices['vBuilder\Config\IConfig']
-		  = array('vBuilder\Config\DbUserConfig', 'createUserConfig');
+/*Nette\Environment::getConfigurator()->defaultServices['vBuilder\Config\IConfig']
+		  = array('vBuilder\Config\DbUserConfig', 'createUserConfig'); */
