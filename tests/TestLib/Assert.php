00806a88dce477a8a579f2dc2da386772875d2a3
Adam Staněk <adam.stanek@v3net.cz>
2011-07-30 22:51:47 +0200

    Priprava na web testy
    Signed-off-by: Adam Staněk <adam.stanek@v3net.cz>

--- tests/TestLib/Assert.php
+++ tests/TestLib/Assert.php
@@ -23,14 +23,46 @@
 
 namespace vBuilder\Test;
 
+use TestCase, Exception;
+
 /**
- * Assert class based on Nette Assert
+ * Assert class based on Nette Assert (c & p because of fucking private methods)
  *
  * @author Adam Staněk (V3lbloud)
  * @since Apr 3, 2011
  */
-class Assert extends \Assert {
+class Assert {
 
+	/**
+	 * Returns message and file and line from call stack.
+	 * @param  string
+	 * @return void
+	 */
+	private static function doFail($message) {
+		global $bluescreen;
+
+		$trace = debug_backtrace();
+		if(defined('HTTP_TEST') && HTTP_TEST) array_pop($trace);
+		$last = end($trace);
+		
+		if(isset($last['line'])) {
+			if($bluescreen = BlueScreen::getInstance()) {
+				$bluescreen->setMessage($message);
+				$bluescreen->setFileBrowser($last['file'], $last['line']);
+			}
+			
+			$message .= " on line $last[line]";
+		}
+		
+		if($bluescreen = BlueScreen::getInstance()) {
+			$bluescreen->render();
+		} else {
+			echo "\n$message";
+		}
+		
+		exit(TestCase::CODE_FAIL); 
+	}
+	
 	/**
 	 * Checks exception assertation.
 	 * @param  string class
@@ -44,7 +76,7 @@ class Assert extends \Assert {
 			// Nemuzu zmenit hlavicku funkce kvuli Strictu, takze to musim obejit
 			// pres dynamicke parametry
 			list($class, $message, $code, $actual) = func_get_args();
-			
+
 			if(is_object($code)) {
 				$actual = $code;
 				$code == null;
@@ -63,11 +95,11 @@ class Assert extends \Assert {
 					self::fail('Failed asserting that exception '.get_class($actual).' with code '.$actual->getCode().' has code '.$code);
 				}
 			}
-			
-		} else
-			return parent::exception($class, $message, $actual);
+		} else {
+			return self::netteException($class, $message, $actual);
+		}
 	}
-	
+
 	/**
 	 * Chacks if associative array is equal to another. Order doesn't matter.
 	 * It also accepts Traversable so DibiRow is Ok.
@@ -79,18 +111,22 @@ class Assert extends \Assert {
 	public static function arrayEqual($expected, $actual, $maxDepth = 5) {
 		if(!is_array($actual) && !($actual instanceof \Traversable))
 			self::fail("Failed asserting that $actual is array equal to $expected: Data given are not even an array");
-		
-		if(!self::arrayEqualHelper($expected, $actual, $maxDepth)) {			
-			echo "EXPECTED: \n";
-			dump($expected);
-			
-			echo "ACTUAL: \n";
-			dump($actual); 
-			
+
+		if(!self::arrayEqualHelper($expected, $actual, $maxDepth)) {
+			if($bluescreen = BlueScreen::getInstance()) {
+				$bluescreen->setAssertVariables($expected, $actual);
+			} else {
+				echo "EXPECTED: \n";
+				dump($expected);
+
+				echo "ACTUAL: \n";
+				dump($actual);
+			}
+
 			self::fail("Failed asserting that $actual is array equal to $expected");
 		}
 	}
-	
+
 	/**
 	 * Chacks if associative array is equal to another. Order doesn't matter.
 	 * It is helper function for arrayEqual which doesn't throw exception but return
@@ -104,11 +140,13 @@ class Assert extends \Assert {
 	 */
 	private static function arrayEqualHelper($expected, $actual, $maxDepth) {
 		// Pokud jsem prekrocil maximalni hloubku
-		if($maxDepth < 0) return true;
-		
+		if($maxDepth < 0)
+			return true;
+
 		// Predpoklada se, ze kdyz je Traversable, tak je i Countable
-		if(count($expected) != count($actual)) return false;
-		
+		if(count($expected) != count($actual))
+			return false;
+
 		// Zjistim, jestli pole je asociativni
 		// Bacha na to, ze to nezohlednuje diry apod, ale nemelo by to vadit
 		if(array_keys($expected) !== range(0, count($expected) - 1)) {
@@ -117,7 +155,7 @@ class Assert extends \Assert {
 					//echo "Missing key $key\n";
 					return false;
 				}
-								
+
 				if(is_array($value) || $value instanceof \Traversable) {
 					if(!self::arrayEqualHelper($value, $actual[$key], $maxDepth - 1))
 						return false;
@@ -125,13 +163,13 @@ class Assert extends \Assert {
 					//echo var_export($value, true) . ' != ' . var_export($actual[$key], true) . "\n";
 					return false;
 				}
-			}			
-			
-		// Pole neni asociativni
+			}
+
+			// Pole neni asociativni
 		} else {
-			foreach($expected as $value) {				
+			foreach($expected as $value) {
 				$found = false;
-				foreach($actual as $value2) {				
+				foreach($actual as $value2) {
 					if(is_array($value) || $value instanceof \Traversable) {
 						if(self::arrayEqualHelper($value, $value2, $maxDepth - 1)) {
 							$found = true;
@@ -142,12 +180,293 @@ class Assert extends \Assert {
 						break;
 					}
 				}
-				
-				if(!$found) return false;
+
+				if(!$found)
+					return false;
 			}
 		}
-		
+
 		return true;
 	}
+
+	// <editor-fold defaultstate="collapsed" desc="Nette methods">
+	
+	/**
+	 * Checks assertion.
+	 * @param  mixed  expected
+	 * @param  mixed  actual
+	 * @return void
+	 */
+	public static function same($expected, $actual) {
+		if($actual !== $expected) {
+			if($bluescreen = BlueScreen::getInstance())
+				$bluescreen->setAssertVariables($expected, $actual);
+			
+			self::log($expected, $actual);
+			self::doFail('Failed asserting that '.self::dump($actual).' is identical to expected '.self::dump($expected));
+		}
+	}
+
+	/**
+	 * Checks assertion.
+	 * @param  mixed  expected
+	 * @param  mixed  actual
+	 * @return void
+	 */
+	public static function equal($expected, $actual) {
+		if($actual != $expected) {
+			if($bluescreen = BlueScreen::getInstance())
+				$bluescreen->setAssertVariables($expected, $actual);
+			
+			self::log($expected, $actual);
+			self::doFail('Failed asserting that '.self::dump($actual).' is equal to expected '.self::dump($expected));
+		}
+	}
+
+	/**
+	 * Checks exception assertion.
+	 * @param  string class
+	 * @param  string message
+	 * @param  Exception
+	 * @return void
+	 */
+	protected static function netteException($class, $message, $actual) {
+		if(!($actual instanceof $class)) {
+			self::doFail('Failed asserting that '.get_class($actual)." is an instance of class $class");
+		}
+		if($message) {
+			self::match($message, $actual->getMessage());
+		}
+	}
+
+	/**
+	 * Checks TRUE assertion.
+	 * @param  mixed  actual
+	 * @return void
+	 */
+	public static function true($actual) {
+		if($actual !== TRUE) {
+			if($bluescreen = BlueScreen::getInstance())
+				$bluescreen->setAssertVariables(true, $actual);
+			
+			self::doFail('Failed asserting that '.self::dump($actual).' is TRUE');
+		}
+	}
+
+	/**
+	 * Checks FALSE assertion.
+	 * @param  mixed  actual
+	 * @return void
+	 */
+	public static function false($actual) {
+		if($actual !== FALSE) {
+			if($bluescreen = BlueScreen::getInstance())
+				$bluescreen->setAssertVariables(false, $actual);
+			
+			self::doFail('Failed asserting that '.self::dump($actual).' is FALSE');
+		}
+	}
+
+	/**
+	 * Checks NULL assertion.
+	 * @param  mixed  actual
+	 * @return void
+	 */
+	public static function null($actual) {
+		if($actual !== NULL) {
+			if($bluescreen = BlueScreen::getInstance())
+				$bluescreen->setAssertVariables(null, $actual);
+			
+			self::doFail('Failed asserting that '.self::dump($actual).' is NULL');
+		}
+	}
+
+	/**
+	 * Checks if the function throws exception.
+	 * @param  callback
+	 * @param  string class
+	 * @param  string message
+	 * @return void
+	 */
+	public static function throws($function, $class, $message) {
+		try {
+			call_user_func($function);
+			self::doFail('Expected exception');
+		} catch(Exception $e) {
+			Assert::exception($class, $message, $e);
+		}
+	}
+
+	/**
+	 * Failed assertion
+	 * @return void
+	 */
+	public static function fail($message) {
+		self::doFail($message);
+	}
+
+	/**
+	 * Initializes shutdown handler.
+	 * @return void
+	 */
+	public static function handler($handler) {
+		ob_start();
+		register_shutdown_function($handler);
+	}
+
+	/**
+	 * Compares results using mask:
+	 *   %a%    one or more of anything except the end of line characters
+	 *   %a?%   zero or more of anything except the end of line characters
+	 *   %A%    one or more of anything including the end of line characters
+	 *   %A?%   zero or more of anything including the end of line characters
+	 *   %s%    one or more white space characters except the end of line characters
+	 *   %s?%   zero or more white space characters except the end of line characters
+	 *   %S%    one or more of characters except the white space
+	 *   %S?%   zero or more of characters except the white space
+	 *   %c%    a single character of any sort (except the end of line)
+	 *   %d%    one or more digits
+	 *   %d?%   zero or more digits
+	 *   %i%    signed integer value
+	 *   %f%    floating point number
+	 *   %h%    one or more HEX digits
+	 *   %ns%   PHP namespace
+	 *   %[..]% reg-exp
+	 * @param  string
+	 * @param  string
+	 * @return bool
+	 */
+	public static function match($expected, $actual) {
+		$expected = rtrim(preg_replace("#[\t ]+\n#", "\n", str_replace("\r\n", "\n", $expected)));
+		$actual = rtrim(preg_replace("#[\t ]+\n#", "\n", str_replace("\r\n", "\n", $actual)));
+
+		$re = strtr($expected, array(
+			 '%a%' => '[^\r\n]+', // one or more of anything except the end of line characters
+			 '%a?%' => '[^\r\n]*', // zero or more of anything except the end of line characters
+			 '%A%' => '.+', // one or more of anything including the end of line characters
+			 '%A?%' => '.*', // zero or more of anything including the end of line characters
+			 '%s%' => '[\t ]+', // one or more white space characters except the end of line characters
+			 '%s?%' => '[\t ]*', // zero or more white space characters except the end of line characters
+			 '%S%' => '\S+', // one or more of characters except the white space
+			 '%S?%' => '\S*', // zero or more of characters except the white space
+			 '%c%' => '[^\r\n]', // a single character of any sort (except the end of line)
+			 '%d%' => '[0-9]+', // one or more digits
+			 '%d?%' => '[0-9]*', // zero or more digits
+			 '%i%' => '[+-]?[0-9]+', // signed integer value
+			 '%f%' => '[+-]?\.?\d+\.?\d*(?:[Ee][+-]?\d+)?', // floating point number
+			 '%h%' => '[0-9a-fA-F]+', // one or more HEX digits
+			 '%ns%' => '(?:[_0-9a-zA-Z\\\\]+\\\\|N)?', // PHP namespace
+			 '%ds%' => '[\\\\/]', // directory separator
+			 '%[^' => '[^', // reg-exp
+			 '%[' => '[', // reg-exp
+			 ']%' => ']+', // reg-exp
+
+			 '.' => '\.', '\\' => '\\\\', '+' => '\+', '*' => '\*', '?' => '\?', '[' => '\[', '^' => '\^', // preg quote
+			 ']' => '\]', '$' => '\$', '(' => '\(', ')' => '\)', '{' => '\{', '}' => '\}', '=' => '\=', '!' => '\!',
+			 '>' => '\>', '<' => '\<', '|' => '\|', ':' => '\:', '-' => '\-', "\x00" => '\000', '#' => '\#',
+				  ));
+
+		$old = ini_set('pcre.backtrack_limit', '1000000');
+		$res = preg_match("#^$re$#s", $actual);
+		ini_set('pcre.backtrack_limit', $old);
+		if($res === FALSE || preg_last_error()) {
+			throw new Exception("Error while executing regular expression.");
+		}
+		if(!$res) {
+			self::log($expected, $actual);
+			self::doFail('Failed asserting that '.self::dump($actual).' matches expected '.self::dump($expected));
+		}
+	}
+
+
+
+	/**
+	 * Dumps information about a variable in readable format.
+	 * @param  mixed  variable to dump
+	 * @return void
+	 */
+	private static function dump($var) {
+		static $tableUtf, $tableBin, $reBinary = '#[^\x09\x0A\x0D\x20-\x7E\xA0-\x{10FFFF}]#u';
+		if($tableUtf === NULL) {
+			foreach(range("\x00", "\xFF") as $ch) {
+				if(ord($ch) < 32 && strpos("\r\n\t", $ch) === FALSE) {
+					$tableUtf[$ch] = $tableBin[$ch] = '\\x'.str_pad(dechex(ord($ch)), 2, '0', STR_PAD_LEFT);
+				} elseif(ord($ch) < 127) {
+					$tableUtf[$ch] = $tableBin[$ch] = $ch;
+				} else {
+					$tableUtf[$ch] = $ch;
+					$tableBin[$ch] = '\\x'.dechex(ord($ch));
+				}
+			}
+			$tableBin["\\"] = '\\\\';
+			$tableBin["\r"] = '\\r';
+			$tableBin["\n"] = '\\n';
+			$tableBin["\t"] = '\\t';
+			$tableUtf['\\x'] = $tableBin['\\x'] = '\\\\x';
+		}
+
+		if(is_bool($var)) {
+			return $var ? 'TRUE' : 'FALSE';
+		} elseif($var === NULL) {
+			return "NULL";
+		} elseif(is_int($var)) {
+			return "$var";
+		} elseif(is_float($var)) {
+			return "$var";
+		} elseif(is_string($var)) {
+			if($cut = @iconv_strlen($var, 'UTF-8') > 100) {
+				$var = iconv_substr($var, 0, 100, 'UTF-8');
+			} elseif($cut = strlen($var) > 100) {
+				$var = substr($var, 0, 100);
+			}
+			return '"'.strtr($var, preg_match($reBinary, $var) || preg_last_error() ? $tableBin
+									 : $tableUtf).'"'.($cut ? ' ...' : '');
+		} elseif(is_array($var)) {
+			return "array(".count($var).")";
+		} elseif($var instanceof Exception) {
+			return 'Exception '.get_class($var).': '.($var->getCode() ? '#'.$var->getCode().' '
+						  : '').$var->getMessage();
+		} elseif(is_object($var)) {
+			$arr = (array) $var;
+			return "object(".get_class($var).") (".count($arr).")";
+		} elseif(is_resource($var)) {
+			return "resource(".get_resource_type($var).")";
+		} else {
+			return "unknown type";
+		}
+	}
+
+	/**
+	 * Logs big variables to file.
+	 * @param  mixed
+	 * @param  mixed
+	 * @return void
+	 */
+	private static function log($expected, $actual) {
+		$trace = debug_backtrace();
+		$item = end($trace);
+		// in case of shutdown handler, we want to skip inner-code blocks
+		// and debugging calls (e.g. those of Nette\Diagnostics\Debugger)
+		// to get correct path to test file (which is the only purpose of this)
+		while(!isset($item['file']) || substr($item['file'], -5) !== '.phpt') {
+			$item = prev($trace);
+			if($item === FALSE) {
+				return;
+			}
+		}
+		$file = dirname($item['file']).'/output/'.basename($item['file'], '.phpt');
+
+		if(is_object($expected) || is_array($expected) || (is_string($expected) && strlen($expected) > 100)) {
+			@mkdir(dirname($file)); // @ - directory may already exist
+			file_put_contents($file.'.expected', is_string($expected) ? $expected : var_export($expected, TRUE));
+		}
+
+		if(is_object($actual) || is_array($actual) || (is_string($actual) && strlen($actual) > 100)) {
+			@mkdir(dirname($file)); // @ - directory may already exist
+			file_put_contents($file.'.actual', is_string($actual) ? $actual : var_export($actual, TRUE));
+		}
+	}
+	
+	// </editor-fold>
 
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -32,12 +32,53 @@ if(!defined('LIBS_DIR')) define('LIBS_DIR', TEST_DIR . '/../..');
 // absolute filesystem path to the app
 if(!defined('APP_DIR')) define('APP_DIR', TEST_DIR . '/../../../app');
 
+if(!defined('TEMP_DIR')) define('TEMP_DIR', APP_DIR . '/../temp/test');
 
-require LIBS_DIR . '/nette/tests/Nette/bootstrap.php';
-require __DIR__ . '/../vBuilderFw/bootstrap.php';
-require __DIR__ . '/TestLib/Assert.php';
-require __DIR__ . '/TestLib/TestException.php';
+require_once LIBS_DIR . '/nette/Nette/loader.php';
+require_once LIBS_DIR . '/nette/tests/Test/TestHelpers.php';
 
+require_once __DIR__ . '/../vBuilderFw/bootstrap.php';
+require_once __DIR__ . '/TestLib/TestScreen.php';
+require_once __DIR__ . '/TestLib/BlueScreen.php';
+require_once __DIR__ . '/TestLib/GreenScreen.php';
+require_once __DIR__ . '/TestLib/Assert.php';
+require_once __DIR__ . '/TestLib/TestException.php';
+
+// configure environment
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', TRUE);
+ini_set('html_errors', FALSE);
+ini_set('log_errors', FALSE);
+
+// create temporary directory and clear its content
+// opravdu je to nutne?
+if(!file_exists(TEMP_DIR) || !file_exists(TEMP_DIR . '/cache')) {
+	TestHelpers::purge(TEMP_DIR);
+}
+
+// catch unexpected errors/warnings/notices
+class TestErrorException extends Exception {}
+
+set_error_handler(function($severity, $message, $file, $line) {
+	if (($severity & error_reporting()) === $severity) {
+		throw new TestErrorException("$message in $file:$line");
+	}
+	return FALSE;
+});
+
+$_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS')));
+$_SERVER['REQUEST_TIME'] = 1234567890;
+$_ENV = $_GET = $_POST = array();
+
+if (PHP_SAPI !== 'cli') {
+	header('Content-Type: text/html; charset=utf-8');
+}
+
+if (extension_loaded('xdebug')) {
+	xdebug_disable();
+	TestHelpers::startCodeCoverage(__DIR__ . '/coverage.dat');
+}
+	
 // Load configuration from config.neon file
 Nette\Environment::loadConfig();
 
