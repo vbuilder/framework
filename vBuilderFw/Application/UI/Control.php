74d80ceed79b041e86751aeb5befcec5d438205b
Adam Staněk <adam.stanek@v3net.cz>
2011-10-07 17:56:10 +0200

    vBuilder Control (prvni koncept)

---
+++ vBuilderFw/Application/UI/Control.php
@@ -0,0 +1,239 @@
+<?php
+
+/**
+ * This file is part of vBuilder Framework (vBuilder FW).
+ * 
+ * Copyright (c) 2011 Adam Staněk <adam.stanek@v3net.cz>
+ * 
+ * For more information visit http://www.vbuilder.cz
+ * 
+ * vBuilder FW is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * vBuilder FW is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with vBuilder FW. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace vBuilder\Application\UI;
+
+use Nette;
+
+/**
+ * Advanced base control for vBuilder based applications
+ *
+ * @author Adam Staněk (velbloud)
+ * @since Oct 7, 2011
+ */
+class Control extends Nette\Application\UI\Control {
+	
+	protected $view = 'default';
+	
+	/** 
+	 * @var vBuilder\Application\UI\ControlRenderer renderer instance
+	 * @internal
+	 */
+	private $_renderer;
+	
+	/**
+	 * Renders control
+	 */
+	function render() {
+		// TODO: dodelat cely workflow
+		
+		// action<View>
+		$actionCalled = $this->tryCall($this->formatActionMethod($this->view), $this->params);
+		
+		// render<View> on renderer instance
+		$this->tryCall($this->formatRenderMethod($this->view), $this->params, $this->renderer);
+		
+		$this->renderer->render();
+	}
+	
+	/**
+	 * Return current context
+	 * 
+	 * @return Nette\DI\IContainer
+	 */
+	final public function getContext() {
+			return $this->getPresenter(true)->context;
+	}
+	
+	/**
+	 * Returns current view
+	 * 
+	 * @return string 
+	 */
+	final public function getView() {
+		return $this->view;
+	}
+	
+	/**
+	 * Returns ORM repository (shortcut)
+	 * 
+	 * @return vBuilder\Orm\Repository
+	 */
+	final public function getRepository() {
+		return $this->context->repository;
+	}
+	
+	// <editor-fold defaultstate="collapsed" desc="Template routines">
+	
+	/**
+	 * @return Nette\Templating\ITemplate
+	 */
+	protected function createTemplate($class = NULL) {
+		if(func_num_args() > 0 && $class != NULL) throw new \InvalidArgumentException(get_called_class() . "::createTemplate do not support $class argument. Take look at renderers instead.");
+		
+		return $this->renderer->template;
+	}
+	
+	/**
+	 * Non sense because of renderers
+	 * @param  Nette\Templating\Template
+	 * @return void
+	 */
+	final public function templatePrepareFilters($template) {
+		throw new \LogicException(get_called_class() . "::templatePrepareFilters should not be called. Use renderers instead.");
+	}
+	
+	// </editor-fold>
+	
+	// <editor-fold defaultstate="collapsed" desc="Renderer routines">
+	
+	final public function setRenderer(ControlRenderer $renderer) {
+		if(isset($this->_renderer))
+			throw new Nette\InvalidStateException("Current renderer (".get_class($this->_renderer).") has been already used. You cannot change it to ".get_class($renderer).".");
+				
+							
+		$this->_renderer = $renderer;
+	}
+	
+	final public function getRenderer() {
+		if(isset($this->_renderer)) return $this->_renderer;
+		
+		$renderer = $this->createRenderer();
+		if($renderer instanceof ControlRenderer) {
+			$this->_renderer = $renderer;
+			return $this->_renderer;
+		} else 
+			throw new \LogicException(get_called_class() . "::createRenderer() has to return child of vBuilder\Application\UI\ControlRenderer.");
+	}
+	
+	protected function createRenderer() {
+		return new ControlRenderer($this);
+	}
+	
+	// </editor-fold>	
+	
+	// <editor-fold defaultstate="collapsed" desc="Signal handling">
+	
+	/**
+	 * Calls signal handler method.
+	 * 
+	 * @param  string
+	 * @return void
+	 * @throws BadSignalException if there is not handler method
+	 */
+	public function signalReceived($signal) {		
+		if(!$this->tryCall($this->formatHandleMethod($signal), $this->params)) {
+			$this->view = $signal;
+			
+			if(!$this->tryCall($this->formatActionMethod($signal), $this->params, $this, true)) {
+				if(!$this->tryCall($this->formatRenderMethod($signal), $this->params, $this->renderer, true)) {
+					$class = get_class($this);
+					throw new Nette\Application\UI\BadSignalException("There is no handler for signal '$signal' in class $class.");
+				}
+			}		
+			
+		}
+	}
+	
+	/**
+	 * Formats action method (action<View>)
+	 * 
+	 * @param  string
+	 * @return string
+	 */
+	public function formatActionMethod($signal) {
+		return $signal == NULL ? NULL : 'action' . $signal; // intentionally ==
+	}
+	
+	/**
+	 * Formats render method (render<View>) - called on renderer
+	 * 
+	 * @param  string
+	 * @return string
+	 */
+	public function formatRenderMethod($signal) {
+		return $signal == NULL ? NULL : 'render' . $signal; // intentionally ==
+	}
+	
+	/**
+	 * Actual signal handle method (handle<Signal>)
+	 * 
+	 * @param  string
+	 * @return string
+	 */
+	public function formatHandleMethod($signal) {
+		return parent::formatSignalMethod($signal);
+	}
+	
+	/**
+	 * Hacked method formatSignalMethod to always point to existing method.
+	 * It is important for correct link creation, but has the down side that
+	 * Presenter::handleInvalidLink will not be called correctly and
+	 * links will be created even for non existing signals.
+	 * 
+	 * @param  string
+	 * @return string
+	 */
+	public function formatSignalMethod($signal)
+	{
+		return $signal == NULL ? NULL : 'signalHandler'; // intentionally ==
+	}
+	
+	/**
+	 * This function is for hacking only. It will never get called because of
+	 * overloaded signalRecieved method.
+	 */
+	final public function signalHandler() {
+		throw new \LogicException(get_called_class() . '::signalHandler() called!');
+	}
+	
+	// </editor-fold>	
+	
+	// <editor-fold defaultstate="collapsed" desc="Helpers">
+	
+	/**
+	 * Calls public method if exists.
+	 * @param  string
+	 * @param  array
+	 * @return bool  does method exist?
+	 */
+	protected function tryCall($method, array $params, $class = null, $dryRun = false) {
+		if (func_num_args() == 2)
+			$class = $this;
+		
+		$rc = $class->getReflection();
+		if ($rc->hasMethod($method)) {
+			$rm = $rc->getMethod($method);
+			if ($rm->isPublic() && !$rm->isAbstract() && !$rm->isStatic()) {
+				$this->checkRequirements($rm);
+				if(!$dryRun) $rm->invokeNamedArgs($class, $params);
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+	
+	// </editor-fold>	
+	
+}
+
---
+++ vBuilderFw/Application/UI/ControlRenderer.php
@@ -0,0 +1,247 @@
+<?php
+
+/**
+ * This file is part of vBuilder Framework (vBuilder FW).
+ * 
+ * Copyright (c) 2011 Adam Staněk <adam.stanek@v3net.cz>
+ * 
+ * For more information visit http://www.vbuilder.cz
+ * 
+ * vBuilder FW is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * vBuilder FW is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with vBuilder FW. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace vBuilder\Application\UI;
+
+use vBuilder,
+		Nette;
+
+/**
+ * Control rendering class
+ *
+ * @author Adam Staněk (velbloud)
+ * @since Oct 7, 2011
+ */
+class ControlRenderer extends vBuilder\Object {
+	
+	/** @var vBuilder\Application\UI\Control control */
+	protected $control;
+	
+	/** 
+	 * @var Nette\Templating\ITemplate template
+	 * @internal
+	 */
+	private $_template;
+	
+	/**
+	 * @var string template directory path (absolute) 
+	 * @internal
+	 */
+	private $_tplDir;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param Control owning control
+	 */
+	function __construct(Control $control) {
+		$this->control = $control;
+	}
+	
+	/**
+	 * Returns current context
+	 * 
+	 * @return Nette\DI\IContainer
+	 */
+	final public function getControl() {
+			return $this->control;
+	}
+	
+	/**
+	 * Returns current context
+	 * 
+	 * @return Nette\DI\IContainer
+	 */
+	final public function getContext() {
+			return $this->control->context;
+	}
+	
+	/**
+	 * Returns the presenter of belonging control.
+	 * @param  bool   throw exception if presenter doesn't exist?
+	 * @return Presenter|NULL
+	 */
+	final public function getPresenter($need = TRUE) {
+		return $this->control->getPresenter($need);
+	}
+	
+	/**
+	 * Returns current control's view
+	 * 
+	 * @return string view name
+	 */
+	final public function getView() {
+		return $this->control->view;
+	}
+	
+	/**
+	 * Default action/view is always defined, so we don't throw an exception
+	 */
+	function renderDefault() {
+		
+	}
+	
+	/**
+	 * Renders matching template for current view
+	 * 
+	 * @return void
+	 */
+	function render() {
+		// File templates
+		if($this->template instanceof Nette\Templating\IFileTemplate && !$this->template->getFile()) {
+			foreach($this->formatTemplateFiles() as $file) {
+				if(file_exists($file)) {
+					$this->template->setFile($file);
+					$this->template->render();
+					return ;
+				}
+			}
+		
+			throw new Nette\Application\BadRequestException("Page not found. Missing template '$file'.");
+		}
+		
+		// Other templates
+		$this->template->render();
+	}
+	
+	/**
+	 * Sets primary template directory
+	 * 
+	 * @param string path to directory (absolute)
+	 */
+	public function setTemplateDirectory($path) {
+		if(!is_dir($path))
+			throw new \InvalidArgumentException("$path is not a directory.");
+		
+		$this->_tplDir = $path;
+	}
+	
+	/**
+	 * Return default template directory (depending on path to class file)
+	 * 
+	 * @return string path to directory 
+	 */
+	protected function getDefaultTemplateDirectory() {
+		return dirname($this->getReflection()->getFileName()) . '/Templates';
+	}
+	
+	/**
+	 * Retursn current template directory
+	 * 
+	 * @return string path to directory
+	 */
+	function getTemplateDirectory() {
+		return isset($this->_tplDir)
+							? $this->_tplDir
+							: $this->getDefaultTemplateDirectory();
+	}
+	
+	/**
+	 * Formats view template file names.
+	 * @return array
+	 */
+	public function formatTemplateFiles() {
+		$filename = $this->view . '.latte';
+		
+		if(isset($this->_tplDir)) {
+			return array(
+					$this->_tplDir . '/' . $filename,
+					$this->getDefaultTemplateDirectory() . '/' . $filename
+			);
+		}
+
+		return array($this->getDefaultTemplateDirectory() . '/' . $filename);
+	}
+	
+	/**
+	 * Returns template
+	 * 
+	 * @return Nette\Templating\ITemplate 
+	 */
+	final public function getTemplate() {
+		if(!isset($this->_template)) {
+			$value = $this->createTemplate();
+			if (!$value instanceof Nette\Templating\ITemplate && $value !== NULL) {
+				$class2 = get_class($value); $class = get_class($this);
+				throw new Nette\UnexpectedValueException("Object returned by $class::createTemplate() must be instance of Nette\\Templating\\ITemplate, '$class2' given.");
+			}
+
+			$this->_template = $value;
+		}
+		
+		return $this->_template;
+	}
+	
+	/**
+	 * Template factory
+	 * 
+	 * @param string class name to use (if null FileTemplate will be used)
+	 * 
+	 * @return Nette\Templating\ITemplate
+	 */
+	protected function createTemplate($class = NULL) {
+		// No need for checking class because of getTemplate
+		$template = $class ? new $class : new Nette\Templating\FileTemplate;
+		$presenter = $this->getPresenter(FALSE);
+		$template->onPrepareFilters[] = callback($this, 'templatePrepareFilters');
+		$template->registerHelperLoader('Nette\Templating\DefaultHelpers::loader');
+
+		// default parameters
+		$template->renderer = $this;
+		$template->control = $this->control;
+		$template->presenter = $presenter;
+		
+		if ($presenter instanceof Presenter) {
+			$template->setCacheStorage($presenter->getContext()->templateCacheStorage);
+			$template->user = $presenter->getUser();
+			$template->netteHttpResponse = $presenter->getHttpResponse();
+			$template->netteCacheStorage = $presenter->getContext()->cacheStorage;
+			$template->baseUri = $template->baseUrl = rtrim($presenter->getHttpRequest()->getUrl()->getBaseUrl(), '/');
+			$template->basePath = preg_replace('#https?://[^/]+#A', '', $template->baseUrl);
+
+			// flash message
+			if ($presenter->hasFlashSession()) {
+				$id = $this->getParamId('flash');
+				$template->flashes = $presenter->getFlashSession()->$id;
+			}
+		}
+		
+		if (!isset($template->flashes) || !is_array($template->flashes)) {
+			$template->flashes = array();
+		}
+
+		return $template;
+	}
+	
+	/**
+	 * Descendant can override this method to customize template compile-time filters.
+	 * @param  Nette\Templating\Template
+	 * @return void
+	 */
+	public function templatePrepareFilters($template) {
+		$engine = new Nette\Latte\Engine;
+		vBuilder\Latte\Macros\RedactionMacros::install($engine->parser);
+		$template->registerFilter($engine);
+	}
+	
+}
