9a0a62b434a9592ee4651829328f8d1531cbe66d
Jirka Vebr <jirkavebr@gmail.com>
2011-12-29 23:12:51 +0100

    Form & Orm helper

--- vBuilderFw/bootstrap.php
+++ vBuilderFw/bootstrap.php
@@ -3,3 +3,7 @@ require_once __DIR__ . '/Utils/shortcuts.php';
 
 // Cestina pro manipulaci s retezci (iconv, etc.)
 setlocale(LC_CTYPE, 'cs_CZ.UTF-8');
+
+Nette\Application\UI\Form::extensionMethod('loadFromEntity', 'vBuilder\Orm\FormHelper::loadFromEntity');
+
+Nette\Application\UI\Form::extensionMethod('saveToEntity', 'vBuilder\Orm\FormHelper::fillInEntity');
