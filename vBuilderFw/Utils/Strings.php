6c4ff509167a0468823470177154a83b3bd11321
Adam Stanek <adam.stanek@v3net.cz>
2012-02-08 15:54:08 +0100

    Strings lib

---
+++ vBuilderFw/Utils/Strings.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of vBuilder Framework (vBuilder FW).
+ * 
+ * Copyright (c) 2011 Adam Staněk <adam.stanek@v3net.cz>
+ * 
+ * For more information visit http://www.vbuilder.cz
+ * 
+ * vBuilder FW is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * vBuilder FW is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with vBuilder FW. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace vBuilder\Utils;
+
+use Nette;
+
+/**
+ * String manipulation library
+ *
+ * @author Adam Staněk (velbloud)
+ * @since Feb 7, 2011
+ */
+class Strings extends Nette\Utils\Strings {
+
+	/**
+	 * Does $haystack contain $needle?
+	 *
+	 * @param string hastack
+	 * @param string needle
+	 * @param bool case sensitive?
+	 *
+	 * @return bool
+	 */
+	public static function contains($haystack, $needle, $caseSensitive = true) {
+		if($caseSensitive)
+			return parent::contains($haystack, $needle);
+		else
+			return parent::contains(self::lower($haystack), self::lower($needle));
+	}
+	
+	/**
+	 * Replaces multiple spaces with one and removes spaces
+	 * from the begining and end of string.
+	 *
+	 * @param string input
+	 * @param bool trim?
+	 * @return string
+	 */
+	public static function simplify($input, $trim = true) {
+		$simplified = preg_replace("/[[:blank:]]+/", " ", $input);
+		
+		return $trim ? trim($simplified) : $simplified;
+	}
+	
+}
