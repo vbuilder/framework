18f07904fcb351b9ad57bb996d32d948140c1b84
Adam Stanek <adam.stanek@v3net.cz>
2012-06-26 10:29:33 +0200

    Diagnostics.OrmSessionBar: Pridan do loaderu

--- vBuilderFw/loader.php
+++ vBuilderFw/loader.php
@@ -7,3 +7,5 @@ setlocale(LC_CTYPE, 'cs_CZ.UTF-8');
 // Formulare: Podpora pro ORM
 Nette\Application\UI\Form::extensionMethod('loadFromEntity', 'vBuilder\Orm\FormHelper::loadFromEntity');
 Nette\Application\UI\Form::extensionMethod('fillInEntity', 'vBuilder\Orm\FormHelper::fillInEntity');
+
+Nette\Diagnostics\Debugger::addPanel(new vBuilder\Diagnostics\OrmSessionBar);
