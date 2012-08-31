a9f1462181c690d1b31e0463a75c8bc35ff145e9
Adam Stanek <adam.stanek@v3net.cz>
2012-08-31 12:52:10 +0200

    Web under construction feature

--- vBuilderFw/loader.php
+++ vBuilderFw/loader.php
@@ -1,4 +1,8 @@
 <?php
+use Nette\Application\Application,
+	Nette\Application\Request,
+	Nette\Utils\Strings;
+
 require_once __DIR__ . '/Utils/shortcuts.php';
 
 // Cestina pro manipulaci s retezci (iconv, etc.)
@@ -9,3 +13,27 @@ Nette\Application\UI\Form::extensionMethod('loadFromEntity', 'vBuilder\Orm\FormH
 Nette\Application\UI\Form::extensionMethod('fillInEntity', 'vBuilder\Orm\FormHelper::fillInEntity');
 
 Nette\Diagnostics\Debugger::addPanel(new vBuilder\Diagnostics\OrmSessionBar);
+
+$container->application->onRequest[] = function (Application $app, Request $request) use ($container) {
+	
+	// Forwardy (jako je napr. presmerovani na error presenter neresim)
+	if($request->method != Request::FORWARD) {
+
+		// Pokud jsem v development modu, tak je vse OK	
+		if(!$container->parameters['productionMode']) return ;
+	
+		
+		// Pokud jsem v produkcnim rezimu, musim zkontrolovat, jestli stranka neni ve vystavbe
+		if(isset($container->parameters['underConstruction']) && $container->parameters['underConstruction'] == true) {
+			
+			// Pokud je stranka ve vystavbe a nejsem na testovaci domene, vyhodim vyjimku
+			// Akceptovany jsou domeny koncici na test.*.* nebo .bilahora.v3net.cz
+			$host = $container->httpRequest->url->host;
+			$host = 'a';
+			if(!Strings::match($host, '#^(.+?\.)?test\.[^\.]+\.[^\.]+$#') && !Strings::match($host, '#\.bilahora\.v3net\.cz$#')) {
+				throw new vBuilder\Application\UnderConstructionException();
+			}
+		}
+	}
+	
+};
