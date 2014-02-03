c4bf7fc1b121c39a91af442875f763967da6c364
Adam Stanek <adam.stanek@v3net.cz>
2014-02-03 13:49:09 +0100

    vBuilderExtension: fixed for Nette v2.1.0

--- vBuilderFw/Config/Extensions/vBuilderExtension.php
+++ vBuilderFw/Config/Extensions/vBuilderExtension.php
@@ -2,11 +2,11 @@
 
 /**
  * This file is part of vBuilder Framework (vBuilder FW).
- * 
+ *
  * Copyright (c) 2011 Adam Staněk <adam.stanek@v3net.cz>
- * 
+ *
  * For more information visit http://www.vbuilder.cz
- * 
+ *
  * vBuilder FW is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -35,11 +35,11 @@ class vBuilderExtension extends Nette\Config\CompilerExtension {
 
 	public function loadConfiguration() {
 		$container = $this->getContainerBuilder();
-		
+
 		// Default language fallback list
 		if(!isset($container->parameters['languages']))
 			$container->parameters['languages'] = array('cs');
-			
+
 		// Default language is the first on the fallback list
 		if(!isset($container->parameters['lang']))
 			$container->parameters['lang'] = reset($container->parameters['languages']);
@@ -50,21 +50,21 @@ class vBuilderExtension extends Nette\Config\CompilerExtension {
 
 		// Translator gets the language from container parameters
 		$container->getDefinition('translator')
-			->addSetup('$service->lang = $this->?[\'lang\']', 'parameters');
-			
+			->addSetup('$service->lang = $this->?[\'lang\']', array('parameters'));
+
 		// Detect language on HTTP request
 		$container->getDefinition('httpRequest')
-			->addSetup('if(($lang = $service->detectLanguage(?*)) != NULL) { $this->parameters[\'lang\'] = $lang; if($this->isCreated(\'translator\')) $translator->setLang($lang); }', array(array('%languages%')));
-	
+			->addSetup('if(($lang = $service->detectLanguage(?*)) != NULL) { $this->parameters[\'lang\'] = $lang; if($this->isCreated(\'translator\')) $translator->setLang($lang); }', array(array($container->expand('%languages%'))));
+
 		// Nette automatically adds Nette\Security\Diagnostics\UserPanel to User service setup
 		// we want to use our own
 		$container->getDefinition('user')->setup = array_filter($container->getDefinition('user')->setup, function ($item) {
-			return $item->entity != 'Nette\Diagnostics\Debugger::$bar->addPanel(?)';
+			return $item->entity != 'Nette\Diagnostics\Debugger::getBar()->addPanel(?)';
 		});
 
 		// Our implementation of Diagnostics\UserPanel
 		if(!$container->parameters['productionMode']) {
-			$container->getDefinition('user')->addSetup('Nette\Diagnostics\Debugger::$bar->addPanel(?)', array(
+			$container->getDefinition('user')->addSetup('Nette\Diagnostics\Debugger::getBar()->addPanel(?)', array(
 				new Nette\DI\Statement('vBuilder\Security\Diagnostics\UserPanel')
 			));
 		}
