ef866bcdcc2a0abb0bacee40e8ea4a017921a015
Adam Stanek <adam.stanek@v3net.cz>
2014-04-28 21:35:26 +0200

    Utils.DateTime: added monthName + translations

--- vBuilderFw/Utils/DateTime.php
+++ vBuilderFw/Utils/DateTime.php
@@ -2,11 +2,11 @@
 
 /**
  * This file is part of vBuilder Framework (vBuilder FW).
- * 
+ *
  * Copyright (c) 2011 Adam Staněk <adam.stanek@v3net.cz>
- * 
+ *
  * For more information visit http://www.vbuilder.cz
- * 
+ *
  * vBuilder FW is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -42,7 +42,7 @@ class DateTime {
 
 	/**
 	 * Returns the first valid DT in given period
-	 * 
+	 *
 	 * @param  \DateTime $dt     [description]
 	 * @param  string vBuilder\Utils\DateTime period constant
 	 * @return \DateTime
@@ -58,7 +58,7 @@ class DateTime {
 				return \DateTime::createFromFormat('Y-m-d H:i:s', $dt->format('Y-m-d H:00:00'), $dt->getTimezone());
 
 			case self::DAY:
-				return \DateTime::createFromFormat('Y-m-d H:i:s', $dt->format('Y-m-d 00:00:00'), $dt->getTimezone());			
+				return \DateTime::createFromFormat('Y-m-d H:i:s', $dt->format('Y-m-d 00:00:00'), $dt->getTimezone());
 			case self::WEEK:
 				$dt2 = \DateTime::createFromFormat('Y-m-d H:i:s', $dt->format('Y-m-d 00:00:00'), $dt->getTimezone());
 				$dt2->modify('-' . ($dt2->format('N') - 1) . ' days');
@@ -77,7 +77,7 @@ class DateTime {
 
 	/**
 	 * Returns the last valid DT of given period
-	 * 
+	 *
 	 * @param  \DateTime $dt     [description]
 	 * @param  string vBuilder\Utils\DateTime period constant
 	 * @return \DateTime
@@ -91,8 +91,8 @@ class DateTime {
 				return $dt2->modify('+59 seconds');
 
 			case self::HOUR:
-				return $dt2->modify('+59 minutes 59 seconds');		
-				
+				return $dt2->modify('+59 minutes 59 seconds');
+
 			case self::DAY:
 				return $dt2->modify('+23 hours 59 minutes 59 seconds');
 
@@ -105,6 +105,63 @@ class DateTime {
 			case self::YEAR:
 				return \DateTime::createFromFormat('Y-m-d H:i:s', $dt->format('Y-12-31 23:59:59'), $dt->getTimezone());
 		}
+	}
+
+	/**
+	 * Returns text representation of month no. N.
+	 *
+	 * If parameter is not valid number between 1 and 12, FALSE is returned.
+	 * If called without parameter array of all months is returned.
+	 *
+	 * @param int 1-12
+	 * @return string|false
+	 */
+	public static function monthName($n = NULL) {
+		$months = array(
+			1 => __('January'),
+				 __('February'),
+				 __('March'),
+				 __('April'),
+				 __('May'),
+				 __('June'),
+				 __('July'),
+				 __('August'),
+				 __('September'),
+				 __('October'),
+				 __('November'),
+				 __('December')
+		);
+
+		if(func_num_args() == 0) return $months;
+		if(!isset($months[$n])) return FALSE;
+
+		return $months[$n];
+	}
+
+	/**
+	 * Returns name of the day of the week
+	 *
+	 * If parameter is not valid number between 1 and 7, FALSE is returned.
+	 * If called without parameter array of all months is returned.
+	 *
+	 * @param int 1-7
+	 * @return string|false
+	 */
+	public static function weekDayName($n = NULL) {
+		$days = array(
+			1 => __('Monday'),
+				 __('Tuesday'),
+				 __('Wednesday'),
+				 __('Thursday'),
+				 __('Friday'),
+				 __('Saturday'),
+				 __('Sunday')
+		);
+
+		if(func_num_args() == 0) return $days;
+		if(!isset($days[$n])) return FALSE;
+
+		return $days[$n];
 	}
 
 }
