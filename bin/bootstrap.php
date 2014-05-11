5ec877a0bedcdfa04afe6681c74acaf6203d7e8e
Adam Stanek <adam.stanek@v3net.cz>
2014-05-11 11:11:17 +0200

    Fixed tr-compile script (boostrap path)

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -2,18 +2,30 @@
 // This file is fake boostrap for our scripts.
 // It searches for real bootstrap in /app directory
 
+if(!isset($bootstrapSearchPath))
+	$bootstrapSearchPath = array();
+
+// Path from ENV variable
+if(isset($_SERVER['APP_DIR']))
+	$bootstrapSearchPath[] = $_SERVER['APP_DIR'] . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+// Default search path
+
 // Note: we don't want to use __DIR__ because composer automatically creates
 // symlinks for scripts (__DIR__ returns actual path).
-$searchPath = array(
+$bootstrapSearchPath[] =
 	dirname(getcwd() . DIRECTORY_SEPARATOR . $argv[0])
-		. DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app'
-		. DIRECTORY_SEPARATOR . 'bootstrap.php'
-);
+	. DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app'
+	. DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-foreach($searchPath as $path) {
+foreach($bootstrapSearchPath as $path) {
 	if(file_exists($path))
 		return require $path;
 }
 
-echo "\nBootstrap not found\n\n";
+echo "\nBootstrap not found\n";
+echo "Search paths:\n";
+echo "\t" . implode($bootstrapSearchPath, "\n\t") . "\n";
+echo "\n";
+
 exit(1);
